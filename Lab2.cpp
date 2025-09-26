570e7d480c6349e81e24b89a36e57d95a4556b86
fedorenkokirilo08-svg <fedorenkokirilo08@gmail.com>
2025-09-26 23:39:46 +0300

    second master commit

--- Lab2.cpp
+++ Lab2.cpp
@@ -18,5 +18,14 @@ int main()
     cout << "z2 = " << z2 << endl; 
 
     cin.get();
+    double a;
+    double x1;
+    double x2;
+	cout << "a = "; cin >> a;
+	x1 = cos(a) + cos(a * 2) + cos(a * 6) + cos(a * 7);
+	x2 = 4 * cos(a/2) * cos((5.0/2.0)* a) * cos(4*a);
+	cout << "x1 = " << x1 << endl;
+	cout << "x2 = " << x2 << endl;
+	cin.get();
     return 0;
 }
